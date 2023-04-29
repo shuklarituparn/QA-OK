0a6dd161b4727fc915c8dce3d0de948464d36bc8
Rituparn Shukla <rituparn_shukla@outlook.com>
2023-04-29 13:46:58 +0300

    added comments

--- src/test/java/tests/pages/LoginFormPage.java
+++ src/test/java/tests/pages/LoginFormPage.java
@@ -10,14 +10,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class LoginFormPage {
-
+                                    //using the allocater we can find element of the page
     private final WebDriver driver;
     By loginField = By.id("field_email");
     By passwordField = By.id("field_password");
 
     public LoginFormPage(WebDriver driver) {
         this.driver = driver;
-        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(loginField));
+        new WebDriverWait(driver, 5).until(visibilityOfElementLocated(loginField));
     }
 
     public void setLogin(String login) {
